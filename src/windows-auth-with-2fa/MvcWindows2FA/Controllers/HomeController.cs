4fa451c21fbcc2e1ff9a8a61759064ddd63fc638
Edd <edd@1n5an1ty.com>
2021-01-11 23:27:52 -0500

    Initial commit

---
+++ src/windows-auth-with-2fa/MvcWindows2FA/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Google.Authenticator;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcWindows2FA.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MvcWindows2FA.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("TwoFactorChallenge")]
+        [AllowAnonymous]
+        public IActionResult TwoFactorChallenge()
+        {
+            TwoFactorAuthenticator tfa = new TwoFactorAuthenticator();
+            var username = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name)?.Value;
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.PrimarySid)?.Value.Replace(" ", "").Replace("-", "");
+            var setupInfo = tfa.GenerateSetupCode("MVC Windows 2FA", username, userId, true);
+
+            string qrCodeImageUrl = setupInfo.QrCodeSetupImageUrl;
+            string manualEntrySetupCode = setupInfo.ManualEntryKey;
+
+            return View(new TwoFactorChallengeViewModel { QrCodeImageUrl = qrCodeImageUrl, ManualEntrySetupCode = manualEntrySetupCode });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/windows-auth-with-2fa/MvcWindows2FA/Models/TwoFactorChallengeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcWindows2FA.Models
+{
+    public class TwoFactorChallengeViewModel
+    {
+        public string QrCodeImageUrl { get; set; }
+        public string ManualEntrySetupCode { get; set; }
+    }
+}
